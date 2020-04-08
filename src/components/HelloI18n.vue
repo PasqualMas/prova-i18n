119a0e881f2a76e8808eb168b2575e3fb44e043c
PasqualMas <57797164+PasqualMas@users.noreply.github.com>
2020-04-08 17:37:44 +0200

    Add files via upload

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <p>{{ $t('hello') }}</p>
+  <p>{{ $t('welcome') }}</p>
+  <button>{{ $t('yes-button') }}</button>
+  <button>{{ $t('no-button') }}</button>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'HelloI18n'
+}
+</script>
+
